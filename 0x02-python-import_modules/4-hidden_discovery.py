56d71be428517b3122372f56273385f49312ca55
EdwadNd <Edwardthina@gmail.com>
2022-12-02 10:59:56 +0000

    refactor pycode

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+import hidden_43
 if __name__ == "__main__":
-	import hidden_4
-
-	for i in dir(hidden_4):
-		if i[:2] == "__":
-			continue
-		print(i)
+    for i in dir(hidden_4):
+	    if i[:2] == "__":
+		    continue
+	    print(i)
