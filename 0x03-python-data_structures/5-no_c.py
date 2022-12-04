b3f459bc475f4adb645f6c588423bc059057e594
EdwadNd <Edwardthina@gmail.com>
2022-12-04 18:05:36 +0000

    string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+	str = ''
+	for i in my_string:
+		if i == ['C' or i == 'c']:
+			continue
+	
+	        new_string = new_string + i
+			    return new_string
+
