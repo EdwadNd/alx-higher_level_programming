099b4af76e246a7b0f145038eda95acf17cd0c0b
EdwadNd <Edwardthina@gmail.com>
2023-01-04 08:32:49 +0000

    class

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)"""
+class Rectangle:
+    """ Rectangle properties """
+    def __init__(self):
+	
+
