f0f60b7e1be2f8cc7f45d864a58a324528d1caeb
Edward Shinyangane Ndlala <Edwardthina@gmail.com>
2023-01-04 10:38:23 +0200

    1 ractangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,7 +1,39 @@
 #!/usr/bin/python3
-"""Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)"""
-class Rectangle:
-    """ Rectangle properties """
-    def __init__(self):
-	
+"""
+Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)
+"""
 
+
+class Rectangle:
+    """ Class To Define a Rectangle with withd and height"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Getter the width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter the value of width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter the value of height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
