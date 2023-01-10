2f84af8a4aaea56ef36bc116aae57bc769002d7c
Edward Ndlala <Edwardthina@gmail.com>
2023-01-10 11:50:27 +0200

    read and write files

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" a function that reads a text file"""
+
+def read_file(filename=""):
+    """ a functiom that prints contents of a file"""
+    with open(filename , encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end="")
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""  a function that writes a string to a text file (UTF8) """
+def write_file(filename="", text=""):
+    """ a function that writes a string to a text file (UTF8) and returns the number of characters written:"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        rite = f.write(text)
+        return rite
