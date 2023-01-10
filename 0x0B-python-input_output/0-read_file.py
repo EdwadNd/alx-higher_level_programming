2a546b261d8827bb3595366839a2f380a59d1c5b
EdwadNd <Edwardthina@gmail.com>
2023-01-10 13:29:52 +0000

    pytest

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ a function that reads a text file"""
 
+
 def read_file(filename=""):
     """ a functiom that prints contents of a file"""
-    with open(filename , mode='r', encoding="utf-8") as f:
+    with open(filename, mode='r', encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """  a function that writes a string to a text file (UTF8) """
+
+
 def write_file(filename="", text=""):
     """ a function that writes a string to a text file (UTF8) and returns the number of characters written:"""
     with open(filename, mode="w", encoding="utf-8") as f:
