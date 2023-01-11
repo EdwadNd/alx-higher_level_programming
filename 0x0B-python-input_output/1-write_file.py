d6bf2f747d6f09703977dca801710a9af90be4b4
EdwadNd <Edwardthina@gmail.com>
2023-01-11 05:38:43 +0000

    pycodestyle

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,8 @@
 
 
 def write_file(filename="", text=""):
-    """ a function that writes a string to a text file (UTF8) and returns the number of characters written:"""
+    """ a function that writes a string to a text file (UTF8)
+    and returns the number of characters written"""
     with open(filename, mode="w", encoding="utf-8") as f:
         rite = f.write(text)
         return rite
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,7 +3,9 @@
 
 
 def append_write(filename="", text=""):
-    """ Write a function that appends a string at the end of a text file (UTF8) and returns the number of characters added"""
+    """ Write a function that appends a string at the end
+    of a text file (UTF8) and
+    returns the number of characters added"""
     with open(filename, mode="a", encoding="utf-8") as f:
         app = f.write(text)
         return app
