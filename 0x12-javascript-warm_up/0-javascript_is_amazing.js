a5afabf43447803037497a4f52edbbc9003c23c3
Edward Ndlala <Edwardthina@gmail.com>
2023-03-13 14:52:43 +0200

    4 tasks

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,3 @@
-let myVar = 'JavaScript is amazing'
+const myVar = 'JavaScript is amazing'
 
 console.log(myVar)
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+const process = require('process')
+const args = process.argv
+
+
+if (args.length === 2) {
+    console.log('No argument')
+  } else if (args.length >= 3) {
+    console.log(args[2])
+  }
