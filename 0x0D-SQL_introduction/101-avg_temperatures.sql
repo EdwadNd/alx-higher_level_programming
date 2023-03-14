403746f547f68320f7bbc64e43cdeb7decc7f847
Edward Ndlala <Edwardthina@gmail.com>
2023-03-14 17:26:44 +0200

    sql adv tasks

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,4 @@
+-- provide temperature
+SELECT city, avg(value) AS avg_temp FROM temperatures 
+GROUP BY city 
+ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,6 @@
+-- Using the same data from ex.18
+-- Display the max temperature of each state
+SELECT state, MAX(value) as max_temp FROM temperatures
+	GROUP BY state
+	ORDER BY state
+	limit 3;
