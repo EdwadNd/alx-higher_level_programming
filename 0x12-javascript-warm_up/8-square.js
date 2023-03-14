0234e343b64c782297eef1530b2d6ec9b5cd8d16
Edward <Edwardthina@gmail.com>
2023-03-14 12:07:17 +0000

    semistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < x; i++) {
     let square = '';
