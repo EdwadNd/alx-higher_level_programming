8eb73cf294e66b955e08dcc3dd0514271baf47ea
Unknown <Edwardthina@gmail.com>
2023-03-14 16:11:32 +0200

    fix

--- 0x13-javascript_objects_scopes_closures/6-Square.js
+++ 0x13-javascript_objects_scopes_closures/6-Square.js
@@ -1,17 +1,19 @@
 #!/usr/bin/node
-const Squarep = require('5-square.js');
-class Square extends Squarep {
+const SquareP = require('./5-square');
+
+class Square extends SquareP {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
-      let square = '';
+      let s = '';
       for (let j = 0; j < this.width; j++) {
-        square += c;
+        s += c;
       }
-      console.log(square);
+      console.log(s);
     }
   }
 }
-module.exports;
+
+module.exports = Square;
