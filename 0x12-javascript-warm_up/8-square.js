3939f2c33faa5f9f47b2f9f4cfe9837edc266363
EdwadNd <Edwardthina@gmail.com>
2023-06-13 02:02:37 +0200

    basic tasks

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    let square = '';
+    for (let j = 0; j < x; j++) {
+      square += 'X';
+    }
+    console.log(square);
+  }
+}
