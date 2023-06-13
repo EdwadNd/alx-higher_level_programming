c87247b892a9f14627cdea26761f700e4ce8694d
EdwadNd <Edwardthina@gmail.com>
2023-06-13 09:21:35 +0000

    all tasked fixedd

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -16,4 +16,16 @@ class Rectangle {
       console.log(square);
     }
   }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = 2 * this.height;
+  }
+}
 module.exports = Rectangle;
