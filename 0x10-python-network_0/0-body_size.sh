28741fa9947111034b3b5a1663be9e8d15b0dc36
Edward Ndlala <edwardthina@gmail.com>
2023-06-29 06:11:39 -0700

    curl operations

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# send a request to an URL using curl
+curl -s "$1" | wc -c
---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Sends a GET request to the URL, and displays the body of the response
+curl -s -L "${1}"
+
