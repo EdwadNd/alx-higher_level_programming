37e5d984d744b67767e338286dd223b19a54f9cb
Edward Ndlala <edwardthina@gmail.com>
2023-08-13 06:24:01 -0700

    creadme me

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
+#!/usr/bin/node
 const fs = require('fs')
  
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
- 
-    console.log(err || data);
-})
+fs.readFile(process.argv[2], 'utf-8', (err, data)=> { 
+	console.log(err || data);
+});
